b7dc476c98ecd04e2c0d64234e72de77a3083913
tvtv <tvtv@tvr.tvr>
2023-12-04 04:29:08 -0500

    Updated the logic of generation of unit details ensuring integrity of data

--- database/factories/UnitFactory.php
+++ database/factories/UnitFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Property;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,9 +17,22 @@ class UnitFactory extends Factory
      */
     public function definition(): array
     {
+        $propertyIdArray = Property::query()->select('id')->orderBy('id','asc')->get()->toArray();
+        do{
+            $propertyID = $this->faker->numberBetween($propertyIdArray[0]['id'], $propertyIdArray[count($propertyIdArray) - 1]['id']);
+
+            // check if property has available units
+            $property = Property::find($propertyID);
+        }while($property->units()->count() >= $property->number_of_units);
+
+        // check if the unit is unique
+        do{
+            $unitName = $this->faker->regexify('[A-Za-z0-9]{3}');
+        }while($property->units()->where('name', $unitName)->count() > 0);
+
         return [
-            'property_id' => $this->faker->numberBetween(1, 10),
-            'name' => $this->faker->regexify('[A-Za-z0-9]{3}'),
+            'property_id' => $propertyID,
+            'name' => $unitName,
             'unit_type_id' => $this->faker->numberBetween(1, 2),
             'created_by' => 1,
         ];
