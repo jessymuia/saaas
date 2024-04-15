3e5d34eeaf547c95bc39269a77462a39aef2dcdf
Ian Kyalo <iankay777@gmail.com>
2024-04-15 12:10:26 +0300

    Commented command to generate billsphp artisan app:add-custom-tenancy-bill-to-invoice

--- app/Console/Commands/AddCustomTenancyBillToInvoice.php
+++ app/Console/Commands/AddCustomTenancyBillToInvoice.php
@@ -27,29 +27,29 @@ class AddCustomTenancyBillToInvoice extends Command
     public function handle()
     {
         //
-        $invoiceId = 6569;
-        $tenancyBill = TenancyBill::create([
-            'invoice_id' => $invoiceId,
-            'tenancy_agreement_id' => 261,
-            'bill_date' => '2024-03-25',
-            'due_date' => '2024-04-05',
-            'amount' => 998.71,
-            'vat' => 159.80,
-            'total_amount' => 1158.51,
-            'billing_type_id' => 1,
-            'utility_id' => 2,
-            'name' => 'March Electricity bill B',
-            'bill_description' => 'March Electricity bill B',
-            'created_by' => 1,
-            'updated_by' => 1,
-            'status' => 1,
-            'archive' => 0
-        ]);
-
-        if ($tenancyBill) {
-            $this->info('Custom tenancy bill added successfully');
-        } else {
-            $this->error('Failed to add custom tenancy bill');
-        }
+//        $invoiceId = 6569;
+//        $tenancyBill = TenancyBill::create([
+//            'invoice_id' => $invoiceId,
+//            'tenancy_agreement_id' => 261,
+//            'bill_date' => '2024-03-25',
+//            'due_date' => '2024-04-05',
+//            'amount' => 998.71,
+//            'vat' => 159.80,
+//            'total_amount' => 1158.51,
+//            'billing_type_id' => 1,
+//            'utility_id' => 2,
+//            'name' => 'March Electricity bill B',
+//            'bill_description' => 'March Electricity bill B',
+//            'created_by' => 1,
+//            'updated_by' => 1,
+//            'status' => 1,
+//            'archive' => 0
+//        ]);
+//
+//        if ($tenancyBill) {
+//            $this->info('Custom tenancy bill added successfully');
+//        } else {
+//            $this->error('Failed to add custom tenancy bill');
+//        }
     }
 }
