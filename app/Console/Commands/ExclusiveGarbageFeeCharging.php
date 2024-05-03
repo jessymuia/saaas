152fbd39d06dc384a930de3e9c73625b9b1b3346
Ian Kyalo <iankay777@gmail.com>
2024-05-03 09:54:10 +0300

    Enabled the billing of services for Wanjeri court

--- app/Console/Commands/ExclusiveGarbageFeeCharging.php
+++ app/Console/Commands/ExclusiveGarbageFeeCharging.php
@@ -74,68 +74,68 @@ class ExclusiveGarbageFeeCharging extends Command
             // display on terminal the id of hte invoice for this tenancy agreement
             $this->info("Invoice id: ". $invoice->id);
 
-//            // get the various services within this property
-//            // generate a bill for each for this month, for this tenancy agreement
-//            $tenancyAgreement->property->propertyServices()->get()->each(/**
-//             * @throws \Exception
-//             */ function ($service) use ($invoice,$billDate,$tenancyAgreement) {
-//                // ensure service bill does not exist for the given month
-//                $serviceBillExists = TenancyBill::query()
-//                    ->where('tenancy_agreement_id', $this->id)
-//                    ->whereMonth('bill_date', '=', $billDate->format('m'))
-//                    ->whereYear('bill_date', trim(date_format($billDate,'Y')))
-//                    ->where('service_id',$service->service_id)
-//                    ->exists();
-//
-//                // TODO: Extra check to prevent backdating of migrated users
-//
-//                if (!$serviceBillExists) {// exit if the service bill exists
-//                    // establish if property is vatable
-//                    $isVatable = $tenancyAgreement->property->property_type_id == 1;
-//                    // create service bill
-//                    $billDueDate = $billDate;
-//
-//                    $isServiceAreaBased = Services::query()
-//                        ->where('id','=',$service->service_id)
-//                        ->value('is_area_based_service');
-//
-//                    if ($isServiceAreaBased){
-//                        // get the area of the unit
-//                        $unitArea = $tenancyAgreement->unit->area_in_square_feet;
-//                        // get the rate of the service
-//                        $serviceRate = $service->rate;
-//                        // calculate the amount
-//                        $serviceAmount = $unitArea * $serviceRate;
-//                        $serviceVat = $serviceAmount * AppUtils::VAT_RATE;
-//                    }else{
-//                        $serviceAmount = $service->rate;
-//                        $serviceVat = $isVatable ? $serviceAmount * AppUtils::VAT_RATE : 0.0;
-//                    }
-//
-//                    $serviceTotalAmount = $serviceAmount + $serviceVat;
-//
-//                    TenancyBill::create([
-//                        'tenancy_agreement_id' => $this->id,
-////                    'name' => $this->tenant->name.' '. TODO: FLAG:MIGRATION removed unnecessary tenant name
-//                        'name' => $billDate->format('F'). ' '.
-//                            Services::query()->where('id','=',$service->service_id)->value('name').
-//                            ' Service Bill',
-////                    'bill_date' => now(),
-//                        'bill_date' => $billDate->format('Y-m-01'), // use the start of the month as the bill date
-//                        'due_date' => $billDueDate->format('Y-m-5'),
-////                    'amount' => $service->rate,
-////                    'vat' => $isVatable ? $service->rate * AppUtils::VAT_RATE : 0.0,
-////                    'total_amount' => $service->rate + ($isVatable ? $service->rate * AppUtils::VAT_RATE : 0.0),
-//                        'amount' => $serviceAmount,
-//                        'vat' => $serviceVat,
-//                        'total_amount' => $serviceTotalAmount,
-//                        'billing_type_id' => $service->billing_type_id,
-//                        'service_id' => $service->service_id,
-//                        'invoice_id' => $invoice->id,
-//                        'created_by' => auth()->user()->id
-//                    ]);
-//                }
-//            });
+            // get the various services within this property
+            // generate a bill for each for this month, for this tenancy agreement
+            $tenancyAgreement->property->propertyServices()->get()->each(/**
+             * @throws \Exception
+             */ function ($service) use ($invoice,$billDate,$tenancyAgreement) {
+                // ensure service bill does not exist for the given month
+                $serviceBillExists = TenancyBill::query()
+                    ->where('tenancy_agreement_id', $this->id)
+                    ->whereMonth('bill_date', '=', $billDate->format('m'))
+                    ->whereYear('bill_date', trim(date_format($billDate,'Y')))
+                    ->where('service_id',$service->service_id)
+                    ->exists();
+
+                // TODO: Extra check to prevent backdating of migrated users
+
+                if (!$serviceBillExists) {// exit if the service bill exists
+                    // establish if property is vatable
+                    $isVatable = $tenancyAgreement->property->property_type_id == 1;
+                    // create service bill
+                    $billDueDate = $billDate;
+
+                    $isServiceAreaBased = Services::query()
+                        ->where('id','=',$service->service_id)
+                        ->value('is_area_based_service');
+
+                    if ($isServiceAreaBased){
+                        // get the area of the unit
+                        $unitArea = $tenancyAgreement->unit->area_in_square_feet;
+                        // get the rate of the service
+                        $serviceRate = $service->rate;
+                        // calculate the amount
+                        $serviceAmount = $unitArea * $serviceRate;
+                        $serviceVat = $serviceAmount * AppUtils::VAT_RATE;
+                    }else{
+                        $serviceAmount = $service->rate;
+                        $serviceVat = $isVatable ? $serviceAmount * AppUtils::VAT_RATE : 0.0;
+                    }
+
+                    $serviceTotalAmount = $serviceAmount + $serviceVat;
+
+                    TenancyBill::create([
+                        'tenancy_agreement_id' => $this->id,
+//                    'name' => $this->tenant->name.' '. TODO: FLAG:MIGRATION removed unnecessary tenant name
+                        'name' => $billDate->format('F'). ' '.
+                            Services::query()->where('id','=',$service->service_id)->value('name').
+                            ' Service Bill',
+//                    'bill_date' => now(),
+                        'bill_date' => $billDate->format('Y-m-01'), // use the start of the month as the bill date
+                        'due_date' => $billDueDate->format('Y-m-5'),
+//                    'amount' => $service->rate,
+//                    'vat' => $isVatable ? $service->rate * AppUtils::VAT_RATE : 0.0,
+//                    'total_amount' => $service->rate + ($isVatable ? $service->rate * AppUtils::VAT_RATE : 0.0),
+                        'amount' => $serviceAmount,
+                        'vat' => $serviceVat,
+                        'total_amount' => $serviceTotalAmount,
+                        'billing_type_id' => $service->billing_type_id,
+                        'service_id' => $service->service_id,
+                        'invoice_id' => $invoice->id,
+                        'created_by' => auth()->user()->id
+                    ]);
+                }
+            });
         }
     }
 }
