1fe98500aa3cb198650b10698971c88237a145f4
Ian Kyalo <iankay777@gmail.com>
2024-07-04 18:14:16 +0300

    Defined the upgrading of rent amount

--- app/Console/Commands/CorrectEscalation.php
+++ app/Console/Commands/CorrectEscalation.php
@@ -29,51 +29,51 @@ class CorrectEscalation extends Command
     public function handle()
     {
         // update the next escalation date for all tenancy agreements where escalation rate is not null
-        \DB::transaction(function () {
-            // verify that the records are 13 before updating
-            $countOfAgreements = TenancyAgreement::query()
-                ->whereNotNull('escalation_rate')
-                ->count();
-            $countOfAgreements == 11
-                ? TenancyAgreement::query()
-                    ->whereNotNull('escalation_rate')
-                    ->chunk(100, function ($tenancyAgreements) {
-                        foreach ($tenancyAgreements as $tenancyAgreement) {
-                            $nextEscalationDate = Carbon::parse($tenancyAgreement->start_date)->addMonths($tenancyAgreement->escalation_period_in_months);
-                            $tenancyAgreement->update(['next_escalation_date' => $nextEscalationDate]);
-                        }
-                    })
-                : $this->info("The number of records is $countOfAgreements");
-        });
+//        \DB::transaction(function () {
+//            // verify that the records are 13 before updating
+//            $countOfAgreements = TenancyAgreement::query()
+//                ->whereNotNull('escalation_rate')
+//                ->count();
+//            $countOfAgreements == 11
+//                ? TenancyAgreement::query()
+//                    ->whereNotNull('escalation_rate')
+//                    ->chunk(100, function ($tenancyAgreements) {
+//                        foreach ($tenancyAgreements as $tenancyAgreement) {
+//                            $nextEscalationDate = Carbon::parse($tenancyAgreement->start_date)->addMonths($tenancyAgreement->escalation_period_in_months);
+//                            $tenancyAgreement->update(['next_escalation_date' => $nextEscalationDate]);
+//                        }
+//                    })
+//                : $this->info("The number of records is $countOfAgreements");
+//        });
 
         // check if the next escalation date is in the past or has passed and escalate the amount by given amount
-//        \DB::transaction(function () {
-//            TenancyAgreement::query()
-//                ->whereNotNull('escalation_rate')
-//                ->where('next_escalation_date', '<', now()->format('Y-m-d'))
-//                ->chunk(100, function ($tenancyAgreements) {
-//                    foreach ($tenancyAgreements as $tenancyAgreement) {
-//                        // calculate new amount
-//                        $newAmount = $tenancyAgreement->amount + ($tenancyAgreement->amount * ($tenancyAgreement->escalation_rate / 100));
-////                        $nextEscalationDate = Carbon::parse($tenancyAgreement->next_escalation_date)->addMonths($tenancyAgreement->escalation_period_in_months);
-//
-//                        // capture the past amount and log it in escalation rates and amounts logs
-//                        EscalationRatesAndAmountsLogs::create(
-//                            [
-//                                'tenancy_agreement_id' => $tenancyAgreement->id,
-//                                'escalation_rate' => $tenancyAgreement->escalation_rate,
-//                                'previous_amount' => $tenancyAgreement->amount,
-//                                'new_amount' => $newAmount,
-//                                'escalation_date' => $tenancyAgreement->next_escalation_date,
-//                                'status' => 1,
-//                                'archive' => 0,
-//                            ]
-//                        );
-//
-//                        // update the tenancy agreement with the new amount and next escalation date
-//                        $tenancyAgreement->update(['amount' => $newAmount]);
-//                    }
-//                });
-//        });
+        \DB::transaction(function () {
+            TenancyAgreement::query()
+                ->whereNotNull('escalation_rate')
+                ->where('next_escalation_date', '<', now()->format('Y-m-d'))
+                ->chunk(100, function ($tenancyAgreements) {
+                    foreach ($tenancyAgreements as $tenancyAgreement) {
+                        // calculate new amount
+                        $newAmount = $tenancyAgreement->amount + ($tenancyAgreement->amount * ($tenancyAgreement->escalation_rate / 100));
+//                        $nextEscalationDate = Carbon::parse($tenancyAgreement->next_escalation_date)->addMonths($tenancyAgreement->escalation_period_in_months);
+
+                        // capture the past amount and log it in escalation rates and amounts logs
+                        EscalationRatesAndAmountsLogs::create(
+                            [
+                                'tenancy_agreement_id' => $tenancyAgreement->id,
+                                'escalation_rate' => $tenancyAgreement->escalation_rate,
+                                'previous_amount' => $tenancyAgreement->amount,
+                                'new_amount' => $newAmount,
+                                'escalation_date' => $tenancyAgreement->next_escalation_date,
+                                'status' => 1,
+                                'archive' => 0,
+                            ]
+                        );
+
+                        // update the tenancy agreement with the new amount and next escalation date
+                        $tenancyAgreement->update(['amount' => $newAmount]);
+                    }
+                });
+        });
     }
 }
